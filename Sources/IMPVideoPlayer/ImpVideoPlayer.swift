2b90c24ca5bd79f8d6a05082a0cbadee789f77a7
mohanlal <mohanlal.shanmugaraj@impigertech.com>
2023-10-23 16:27:12 +0530

    init

---
+++ Sources/IMPVideoPlayer/ImpVideoPlayer.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by mohanlal on 23/10/23.
+//
+import SwiftUI
+import AVKit
+
+@available(iOS 14.0, *)
+struct ImpVideoPlayer: View {
+    @Binding var videoUrl : String
+    var body: some View {
+        VStack {
+            VideoPlayer(player: AVPlayer(url: URL(string: videoUrl)!))
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+@available(iOS 14.0, *)
+#Preview {
+    ImpVideoPlayer(videoUrl: .constant("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"))
+}
